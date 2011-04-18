05e63c622bdd76c093c27f0fb4b206fe2d9df3cb
fakestarbaby <fakestarbaby@gmail.com>
2011-04-19 08:27:40 +0900

    初回コミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleEdgeRails::Application.config.secret_token = 'b67d43a5ae896133102ddefcc83d236009122d9851415125309780f15709dad16fe517b7261feeb53a2b210b4e92adb7d7b8da3223e0e72e72a0456304ed2394'
